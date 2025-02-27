90d56eed6bf56eeafac4a82691f6fd1d68a5a2e2
Ankit161409 <ankit161210@gmail.com>
2025-02-27 18:44:31 +0530

    Add files via upload

---
+++ star patters/pattern10.java
@@ -0,0 +1,31 @@
+                                            /*
+ # # # # # # # # # # #
+    # # # # # # # # #
+      # # # # # # #
+        # # # # #
+          # # #
+            #       
+		                                  	*/
+
+class child
+{
+public static void main(String args[])
+{int z=11;
+
+	for (int i=1; i<=6; i++)
+	{
+		for (int k=1; k<=i; k++)
+		{
+			System.out.print("  ");
+		}
+			for(int j=1; j<=z; j++)
+			
+			{ 
+			System.out.print("# ");
+			} 
+			z-=2;
+			System.out.println( );
+	}
+}
+}
+			
